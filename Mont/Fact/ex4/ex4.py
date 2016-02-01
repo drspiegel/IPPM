c9326f5ba78a96221ee664178f0e47ec916d6056
drspiegel <rf170c@gmail.com>
2016-02-01 16:06:07 +0300

    Update ex4.py

--- Mont/Fact/ex4/ex4.py
+++ Mont/Fact/ex4/ex4.py
@@ -5,19 +5,21 @@ import sys
 import primesieve
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print ("Error: Too few parameters in {0}, expected 1.".format(sys.argv[0]))
+    if len(sys.argv) < 2:
+        print ("Error: Too few arguments in {0}, expected 1.".format(sys.argv[0]))
         sys.exit(1)
-    else:
-        dr = eval(sys.argv[1])
-        print("DR = eval('{0}') = {1}".format(sys.argv[1], dr))
-        pr = []
-        P = 1
-        it = primesieve.Iterator()
-        it.next_prime() # to ignore 2
+    if len(sys.argv) > 2:
+        print ("Error: Too many arguments in {0}, expected 1.".format(sys.argv[0]))
+        sys.exit(1)
+    dr = eval(sys.argv[1])
+    print("DR = eval('{0}') = {1}".format(sys.argv[1], dr))
+    pr = []
+    P = 1
+    it = primesieve.Iterator()
+    it.next_prime() # to ignore 2
 
-        while (P < dr):
-            pi = it.next_prime()
-            pr.append(str(pi))
-            P *= pi
-        print("P = {1} = {0}".format(P, ' * '.join(pr)))
+    while (P < dr):
+        pi = it.next_prime()
+        pr.append(str(pi))
+        P *= pi
+    print("P = {1} = {0}".format(P, ' * '.join(pr)))
