7979c4cc08a223ec80c55d5c7453aecbbc0d9e13
drspiegel <rf170c@gmail.com>
2016-02-25 15:36:28 +0300

    Update montg.py

--- Mont/montg.py
+++ Mont/montg.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
 import random
-
-def bitsize (x): # return a bit size of a non-negative (>=0) integer number
-    return(len(bin(x))-2)
+import gmpy2
 
 def mod (a, b):
     return a % b
@@ -24,111 +21,59 @@ def modinv(a, m):
     else:
         return x % m
 
-def montgPro(a, b, n):
-    s = bitsize(n)
-    r = pow(2, s)
-    #print("r={0};".format(r), end=' ')
-    if not r > n:
-        print("Error: bad r!")
-        sys.exit(1)
-
-    r_ = modinv(r, n)
-    n1 = modinv(n, r)
-
-    if mod(r * r_, n) != 1:
-        print("Error: bad r_!")
-        sys.exit(1)
-    if mod(n * n1, r) != 1:
-        print("Error: bad n1!")
-        sys.exit(1)
-
-    n_ = r - n1
-    a_ = mod(a * r, n)
-    b_ = mod(b * r, n)
-
+def MonPro (a_, b_): # Montgomery multiplication calc. (a_* b_) * r_ mod n
+    global n
+    global n_
+    global r
     t = a_ * b_
-    m = mod(t * n_, r)
-    t = t + m * n
-    t = t // r
-
-    return mod(t * r_, n)
-
-def mm (a_, b_, n, n_, r):
-    t = a_ * b_
-    m = mod(t * n_, r)
-    t = t + m * n
-    u = t // r
+    u = (t + mod(t * n_, r) * n) // r
     if (u >= n):
         return u - n
     else:
         return u
 
-def montgExp(a, e, n):
-    s = bitsize(n)
-    r = pow(2, s)
-    #print("r={0};".format(r), end=' ')
-    if not r > n:
-        print("Error: bad r!")
-        sys.exit(1)
-
-    r_ = modinv(r, n)
-    n1 = modinv(n, r)
-
-    if mod(r * r_, n) != 1:
-        print("Error: bad r_!")
-        sys.exit(1)
-    if mod(n * n1, r) != 1:
-        print("Error: bad n1!")
-        sys.exit(1)
-
-    n_ = r - n1
+def ModExp (a, e, n): # Modular exponentiation with MonPro
+    global r
     a_ = mod(a * r, n)
-    # b_ = mod(b * r, n)
     x_ = mod(1 * r, n)
+    j = (e.bit_length() or 1)
+    e = gmpy2.xmpz(e)
+    for i in range(j-1, -1, -1): # loop on i from j-1 downto 0
+        x_ = MonPro(x_, x_)
+        if (e[i] == 1):
+            x_ = MonPro(x_, a_)
+    return MonPro(x_, 1)  # mod(x_ * r_, n)
 
-    oo = bin(e)
-    #print(e, oo)
-    j = bitsize(e)
-    for i in range(j-1, -1, -1): # for from j-1 to 0
-        x_ = mm(x_, x_, n, n_, r)
-        #print(i)
+if __name__ == "__main__":
+    N = 3, 7, 11, 29, 59, 107, 239
+    random.seed()
+    for n in N:
 
+        # input: 0 <= a, b < n; r=(2**s) > n; gcd(r, n) = 1
+        s = (n.bit_length() or 1)
+        r = pow(2, s)
 
-        if (int(oo[i+2]) == 1):
-            #print(i, i+2, oo[i+2])
-            x_ = mm(x_, a_, n, n_, r)
-    x_ = mm(x_, 1, n, n_, r)
+        # r * r_ - n * n_ = gcd(r, n) = 1
+        r_ = modinv(r, n)
+        n1 = modinv(n, r)
+        n_ = r - n1
 
-    return mod(x_ * r_, n)
+        a = random.randint(0, n-1)
+        e = random.randint(0, n-1)
+        x = ModExp(a, e, n)
+        orig = pow(a, e, n)
+        if x != orig:
+            print("Error-B: n={0}; a={1}; e={2}; x={3}; orig={4};".format(n, a, e, x, orig))
 
+        a = random.randint(0, n-1)
+        b = random.randint(0, n-1)
 
-    #return x
-    #return mod(x * r_, n)
+        # transformation of a and b
+        a_ = mod(a * r, n)
+        b_ = mod(b * r, n)
 
-# 2 - 4 3
-# 4 - 8 7
-# 8 - 16 11
-# 16 - 32 29
-# 32 - 64 59
-# 64 - 128 107
-# 128-256 239
-
-N = 3, 7, 11, 29, 59, 107, 239
-print(N)
-
-random.seed()
-
-for n in N:
-    a = random.randint(0, n-1)
-    b = random.randint(0, n-1)
-
-    v = montgPro(a, b, n)
-    orig = (a*b) % n
-    if v != orig:
-        print("Error-A: n={0}; a={1}; b={2}; v={3}; orig={4};".format(n, a, b, v, orig))
-    e = b
-    v = montgExp(a, e, n)
-    orig = pow(a, e) % n
-    if v != orig:
-        print("Error-B: n={0}; a={1}; e={2}; v={3}; orig={4};".format(n, a, e, v, orig))
-
+        x_ = MonPro(a_, b_)
+        x = MonPro(x_, 1)
+        orig = (a * b) % n
+        if x != orig:
+            print("Error-A: n={0}; a={1}; b={2}; x={3}; orig={4};".format(n, a, b, x, orig))
