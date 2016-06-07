cce72b057f5059c10fa0c663f707f98fb7f22968
drspiegel <rf170c@gmail.com>
2016-06-07 17:16:39 +0200

    Create montg4.py

---
+++ Mont/montg4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+import gmpy2
+
+start_bit, finish_bit = (2, 3)
+radix = 2 base of 
+
+if __name__ == "__main__":
+    for n in range(pow(radix, start_bit), pow(radix, finish_bit)):
+        if (n % 2 == 0):
+            continue
+        print("Output: n={0}".format(n))
+
+        http://younglinux.info/python/task/even-odd
+        https://habrahabr.ru/post/122538/
+        http://forum.sources.ru/index.php?showtopic=348429
+        http://pythonworld.ru/moduli/modul-random.html
+        
